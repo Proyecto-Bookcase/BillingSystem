58a97e3369ab5ac5167adba51b8c45549eec2a61
Diego Alfonso Criado <dialcr45@gmail.com>
2023-12-08 15:28:58 -0500

    Add Services Location,Warehose¡

---
+++ src/Entity/Location.java
@@ -0,0 +1,80 @@
+package Entity;
+
+public class Location {
+    private int compartment;
+    private int floor;
+    private int shelf;
+    private boolean maintenance;
+    private int warehouseNumber;
+    private int cargoId;
+
+    public Location(int compartment, int floor, int shelf, boolean maintenance, int warehouseNumber, int cargoId) {
+        this.compartment = compartment;
+        this.floor = floor;
+        this.shelf = shelf;
+        this.maintenance = maintenance;
+        this.warehouseNumber = warehouseNumber;
+        this.cargoId = cargoId;
+    }
+
+    public int getCompartment() {
+        return compartment;
+    }
+
+    public void setCompartment(int compartment) {
+        this.compartment = compartment;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getShelf() {
+        return shelf;
+    }
+
+    public void setShelf(int shelf) {
+        this.shelf = shelf;
+    }
+
+    public boolean isMaintenance() {
+        return maintenance;
+    }
+
+    public void setMaintenance(boolean maintenance) {
+        this.maintenance = maintenance;
+    }
+
+    public int getWarehouseNumber() {
+        return warehouseNumber;
+    }
+
+    public void setWarehouseNumber(int warehouseNumber) {
+        this.warehouseNumber = warehouseNumber;
+    }
+
+    public int getCargoId() {
+        return cargoId;
+    }
+
+    public void setCargoId(int cargoId) {
+        this.cargoId = cargoId;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "compartment=" + compartment +
+                ", floor=" + floor +
+                ", shelf=" + shelf +
+                ", maintenance=" + maintenance +
+                ", warehouseNumber=" + warehouseNumber +
+                ", cargoId=" + cargoId +
+                '}';
+    }
+}
+
