476624c920dcc52493c9a2f0635f8b91d1b7876a
Altro <albe020531@outlook.com>
2023-12-09 20:32:17 -0500

    Scene Mnager implementation

---
+++ src/javafx/utils/others/Auxiliary.java
@@ -0,0 +1,22 @@
+package javafx.utils.others;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Contain all auxiliary functions that has no defined classification
+ */
+public abstract class Auxiliary {
+
+    /**
+     *
+     * @param callingOrder if passed, increment the order of the calling function.
+     * @return Name of the calling function
+     */
+    public static @NotNull String getCallingFunctionName(int...callingOrder){
+
+        int callIncrement = callingOrder.length > 0? 3 + callingOrder[0] : 3;
+
+        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+        return elements[callIncrement].getMethodName();
+    }
+}
