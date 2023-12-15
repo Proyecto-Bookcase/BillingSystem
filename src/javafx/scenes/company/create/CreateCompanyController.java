f340d16f25c5fc4a96d17968410eb7bbd7951842
Altro <albe020531@outlook.com>
2023-12-15 02:11:51 -0500

    Implemented Companies functionality

--- src/javafx/scenes/company/create/CreateCompanyController.java
+++ src/javafx/scenes/company/create/CreateCompanyController.java
@@ -1,21 +1,36 @@
 package javafx.scenes.company.create;
 
+import Dtos.CompanyTypeDto;
+import Dtos.ConditioningCompanyDto;
+import Dtos.HandlingGoodsDto;
+import Dtos.PriorityCompanyDto;
 import io.github.palexdev.materialfx.controls.*;
-import io.github.palexdev.materialfx.controls.models.spinner.SpinnerModel;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.input.InputMethodEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scenes.home.subscenes.manager.HomeSceneManager;
+import javafx.util.StringConverter;
+import javafx.utils.components.spinner.FloatSpinnerModel;
+import javafx.utils.scene_manager.SceneManager;
+import javafx.utils.scene_manager.Scenes;
+import services.*;
 
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import static javafx.utils.async.thread.ThreadHelpers.fxthread;
+import static javafx.utils.async.thread.ThreadHelpers.thread;
+
 public class CreateCompanyController implements Initializable {
+
+    private final EnterpriseServices enterpriseServices = ServicesLocator.getEnterpriseServices();
+    private final CompanyServices companyServices = ServicesLocator.getCompanyServices();
+    private final CompanyTypeServices ctypeServices = ServicesLocator.getCompanyTypeServices();
+    private final HandlingGoodsServices hgServices = ServicesLocator.getHandlingGoodsServices();
+    private final PriorityCompanyServices priorityServices = ServicesLocator.getPriorityCompanyServices();
+    private final ConditioningCompanyServices conditioningServices = ServicesLocator.getConditioningCompanyServices();
+
     @FXML
     public AnchorPane pane;
 
@@ -24,15 +39,15 @@ public class CreateCompanyController implements Initializable {
     @FXML
     private MFXTextField name;
     @FXML
-    private MFXComboBox<String> type;
+    private MFXComboBox<CompanyTypeDto> type;
     @FXML
     private MFXSpinner<Float> fuel_tariff;
     @FXML
-    private MFXCheckListView<String> handling_goods;
+    private MFXCheckListView<HandlingGoodsDto> handling_goods;
     @FXML
-    private MFXCheckListView<String> priority;
+    private MFXCheckListView<PriorityCompanyDto> priority;
     @FXML
-    private MFXCheckListView<String> conditioning;
+    private MFXCheckListView<ConditioningCompanyDto> conditioning;
 
 
     /**
@@ -47,30 +62,104 @@ public class CreateCompanyController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        // TODO Implementar inicializador de Handling Goods
-        handling_goods.getItems().addAll( /* Aquí */ );
+        fuel_tariff.setSpinnerModel(new FloatSpinnerModel(0f, null, 0.5f));
 
-        // TODO Implementar inicializador de Priority
-        priority.getItems().addAll( /* Aquí */ );
+        type.setConverter(new StringConverter<CompanyTypeDto>() {
+            @Override
+            public String toString(CompanyTypeDto object) {
+                return object.getDescription();
+            }
 
-        // TODO Implementar inicializador de Conditioning
-        conditioning.getItems().addAll( /* Aquí */ );
+            @Override
+            public CompanyTypeDto fromString(String string) {
+                return type.getItems().filtered(cType -> cType.getDescription().equals(string)).get(0);
+            }
+        });
+
+        handling_goods.setConverter(new StringConverter<HandlingGoodsDto>() {
+            @Override
+            public String toString(HandlingGoodsDto object) {
+                return object.getDescription();
+            }
+
+            @Override
+            public HandlingGoodsDto fromString(String string) {
+                return handling_goods.getItems().stream().filter((hg -> hg.getDescription().equals(string))).toList().get(0);
+            }
+        });
+
+        priority.setConverter(new StringConverter<PriorityCompanyDto>() {
+            @Override
+            public String toString(PriorityCompanyDto object) {
+                return object.getDescription();
+            }
+
+            @Override
+            public PriorityCompanyDto fromString(String string) {
+                return priority.getItems().stream().filter((hg -> hg.getDescription().equals(string))).toList().get(0);
+            }
+        });
+
+        conditioning.setConverter(new StringConverter<ConditioningCompanyDto>() {
+            @Override
+            public String toString(ConditioningCompanyDto object) {
+                return object.getDescription();
+            }
+
+            @Override
+            public ConditioningCompanyDto fromString(String string) {
+                return conditioning.getItems().stream().filter((hg -> hg.getDescription().equals(string))).toList().get(0);
+            }
+        });
+
+        thread(() -> {
+            handling_goods.getItems().addAll(hgServices.getAllHandlingGoodsCompany());
+        });
+
+        thread(() -> {
+            priority.getItems().addAll(priorityServices.getAllPriorityCompany());
+        });
+
+        thread(() -> {
+            conditioning.getItems().addAll(conditioningServices.getAllConditioningCompany());
+        });
+
+        thread(() -> {
+            type.getItems().addAll(ctypeServices.getAllCompanyType());
+        });
     }
 
     @FXML
     private void onSubmit() {
 
         String cname = name.getText();
-        String ctype = type.getValue();
+        CompanyTypeDto ctype = type.getValue();
         float cfuel_tariff = fuel_tariff.getValue();
-        List<String> chandling_goods = handling_goods.getSelectionModel().getSelectedValues();
-        List<String> cpriority = priority.getSelectionModel().getSelectedValues();
-        List<String> cconditioning = conditioning.getSelectionModel().getSelectedValues();
+        Integer[] chandling_goods = handling_goods.getSelectionModel().getSelectedValues().stream().map(HandlingGoodsDto::getId).toList().toArray(new Integer[0]);
+        Integer[] cpriority = priority.getSelectionModel().getSelectedValues().stream().map(PriorityCompanyDto::getId).toList().toArray(new Integer[0]);
+        Integer[] cconditioning = conditioning.getSelectionModel().getSelectedValues().stream().map(ConditioningCompanyDto::getId).toList().toArray(new Integer[0]);
+
+
+        thread(() -> {
+            companyServices.insertCompany(
+                    cname,
+                    cfuel_tariff,
+                    ctype.getId(),
+                    enterpriseServices.getEnterpirseDbFunction(6).getId(),
+                    cconditioning,
+                    chandling_goods,
+                    cpriority
+            );
+            fxthread(() -> {
+                SceneManager.show(Scenes.HOME);
+                HomeSceneManager.to(javafx.scenes.home.subscenes.manager.Scenes.COMPANIES);
+            });
+        });
 
     }
 
     @FXML
-    public void onFuelTariffTextChange(){
+    public void onFuelTariffTextChange() {
         System.out.println("Altro");
     }
 }
